de933556a2aa9a6c4bdc20998885ae4028939281
kachiem <kachiem@ucsd.edu>
2019-02-07 10:52:12 -0800

    that's a yikes from me

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,26 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	console.log(data);
+	res.render('index', {
+		'name': data.name,
+		'imageURL' : data.imageURL,
+		'description' : data.description
+	});
+	res.query.name();
+	res.query.description();
+	console.log("that's a yikes from me");
+
  }
+
+{
+	"newFriend": [
+		{
+			"name": "Doug Engelbart",
+			"description": "Has awesome pet mice",
+			"imageURL": "http://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Douglas_Engelbart_in_2008.jpg/972px-Douglas_Engelbart_in_2008.jpg"			
+		},
+	]
+}
+ 
+data.friends.push(newFriend);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 // Get all of our friend data
 var data = require('../data.json');
+//app.get("/data/:userName", );
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', {
+		'name': data.name,
+		'imageURL' : data.imageURL,
+		'description' : data.description
+	});
+	//console.log("got here");
 };
